51d8b6c9ef086fbdcaca4d4985252519060e44f5
Mustafa Hassanein <mustafadabsha@yahoo.com>
2022-12-26 21:29:01 +0000

    Saved progress on IfStatement 2022-12-26 21:29:01 +0000

--- IfStatement/src/main/java/IfStatement.java
+++ IfStatement/src/main/java/IfStatement.java
@@ -17,14 +17,14 @@ public class IfStatement {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return true; 
+        return y; 
         int x = 8;
-        int y = 6; 
-        boolean bool = true;
-        if(x > y){
-            bool = true;
-        }else if(x < y){
-            bool= false; 
+        int y = 6;  
+
+        if (x > y){
+        boolean bool = true;            
+        }else if(y > x){
+        boolean bool = false; 
 }
 } 
     
