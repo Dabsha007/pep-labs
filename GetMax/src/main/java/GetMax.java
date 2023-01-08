5580600cdae8d065a2ca78f0d272b864c008087f
Mustafa Hassanein <mustafadabsha@yahoo.com>
2023-01-08 15:39:27 +0000

    Saved progress on Average 2023-01-08 15:39:27 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -7,8 +7,8 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        int max =0;
-        for(int i =0; i<max.length; i--){
+        int max = 0;
+        for(int i =0; i< max.length; i--){
         max = max + arr[i];
         }
         return -1;
