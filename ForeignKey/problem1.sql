d6653d9d7ff4d25c7bb05cc7782b8ac31bed4f44
Mustafa Hassanein <mustafadabsha@yahoo.com>
2023-02-08 02:29:32 +0000

    Saved progress on ForeignKey 2023-02-08 02:29:32 +0000

--- ForeignKey/problem1.sql
+++ ForeignKey/problem1.sql
@@ -1,1 +1,5 @@
-CREATE TABLE post(id SERIAL PRIMARY KEY, post_name varchar(255), user_fk int REFERENCES post(id) ); 
+CREATE TABLE post(
+    id SERIAL PRIMARY KEY, 
+    post varchar(255), 
+    user_fk int REFERENCES site_user(id)
+); 
